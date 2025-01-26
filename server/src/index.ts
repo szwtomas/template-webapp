879b91039530c0a4391d77dfd75ed7db48cb3022
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2025-01-26 17:24:00 -0300

    Adds dev scripts and mock server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/health", (_req, res) => {
+    res.status(200).send("0");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
